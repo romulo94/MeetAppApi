bf9c97bf267b2ddf23520e28e3e336d8407d12cb
Romulo Rocha <romulorocha063@gmail.com>
2019-08-23 22:47:53 -0300

    Removed all try-catch in controllers

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -5,48 +5,44 @@ import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
-    try {
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .email()
-          .required(),
-        password: Yup.string()
-          .required()
-          .min(3),
-      });
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(3),
+    });
 
-      if (!(await schema.isValid(req.body))) {
-        return res.status(400).json({ error: 'Validation fails' });
-      }
-
-      const { email, password } = req.body;
-      const user = await User.findOne({
-        where: {
-          email,
-        },
-      });
-
-      if (!user) return res.status(404).json({ error: 'User not found' });
-
-      const passwordChecked = await user.checkPassword(password);
-
-      if (!passwordChecked)
-        return res.status(401).json({ error: 'Password does not match' });
-
-      const { id, name } = user;
-      const token = jwt.sign({ id }, authConfig.secret, {
-        expiresIn: authConfig.expiresIn,
-      });
-
-      return res.json({
-        id,
-        name,
-        email,
-        token,
-      });
-    } catch (error) {
-      return res.status(500).json({ error: 'Internal server error' });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
     }
+
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    const passwordChecked = await user.checkPassword(password);
+
+    if (!passwordChecked)
+      return res.status(401).json({ error: 'Password does not match' });
+
+    const { id, name } = user;
+    const token = jwt.sign({ id }, authConfig.secret, {
+      expiresIn: authConfig.expiresIn,
+    });
+
+    return res.json({
+      id,
+      name,
+      email,
+      token,
+    });
   }
 }
 
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,91 +3,79 @@ import User from '../models/User';
 
 class UserController {
   async index(req, res) {
-    try {
-      const users = await User.findAll({
-        attributes: ['id', 'name', 'email'],
-      });
+    const users = await User.findAll({
+      attributes: ['id', 'name', 'email'],
+    });
 
-      return res.json(users);
-    } catch (error) {
-      return res.status(500).json({ error: 'Internal server error' });
-    }
+    return res.json(users);
   }
 
   async store(req, res) {
-    try {
-      const schema = Yup.object().shape({
-        name: Yup.string().required(),
-        email: Yup.string()
-          .email()
-          .required(),
-        password: Yup.string()
-          .required()
-          .min(3),
-      });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(3),
+    });
 
-      if (!(await schema.isValid(req.body))) {
-        return res.status(400).json({ error: 'Validation fails' });
-      }
-
-      const { id, name, email } = await User.create(req.body);
-
-      return res.json({
-        id,
-        name,
-        email,
-      });
-    } catch (error) {
-      return res.status(500).json({ error: 'Internal server error' });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
     }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 
   async update(req, res) {
-    try {
-      const schema = Yup.object().shape({
-        name: Yup.string(),
-        email: Yup.string().email(),
-        oldPassword: Yup.string().min(3),
-        password: Yup.string()
-          .min(3)
-          .when('oldPassword', (oldPassword, field) =>
-            oldPassword ? field.required() : field
-          ),
-        confirmPassword: Yup.string().when('password', (password, field) =>
-          password ? field.required().oneOf([Yup.ref('password')]) : field
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(3),
+      password: Yup.string()
+        .min(3)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
         ),
-      });
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
 
-      if (!(await schema.isValid(req.body))) {
-        return res.status(400).json({ error: 'Validation fails' });
-      }
-
-      const { email, oldPassword } = req.body;
-
-      const user = await User.findByPk(req.userId);
-
-      if (oldPassword && !(await user.checkPassword(oldPassword))) {
-        return res.status(401).json({ error: 'Password does not match' });
-      }
-
-      if (email && email !== user.email) {
-        const userExist = await User.findOne({ where: { email } });
-
-        if (userExist) {
-          return res.status(409).json({ error: 'Email already exists' });
-        }
-      }
-
-      const { id, name } = await user.update(req.body);
-
-      return res.json({
-        id,
-        name,
-        email,
-      });
-    } catch (error) {
-      return res.status(500).json({ error: 'Internal server error' });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
     }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    if (email && email !== user.email) {
+      const userExist = await User.findOne({ where: { email } });
+
+      if (userExist) {
+        return res.status(409).json({ error: 'Email already exists' });
+      }
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 }
 
